263028f293613f5c2acebfd0b86a2025da5c3f7e
rafalcl <rafalclab@gmail.com>
2018-04-27 16:47:04 +0200

    Add solution and user model classes

---
+++ src/pl/coderslab/warsztaty3/model/Solution.java
@@ -0,0 +1,8 @@
+package pl.coderslab.warsztaty3.model;
+
+public class Solution {
+	private String solutionText;
+	private long userId;
+	private long exerciseId;
+	// konstruktory gettery settery
+}
---
+++ src/pl/coderslab/warsztaty3/model/User.java
@@ -0,0 +1,8 @@
+package pl.coderslab.warsztaty3.model;
+
+public class User {
+	private String name;
+	private String email;
+	// konstruktory
+	// gettery settery
+}

fb1fc26cddefa9a91003b7a0ec3b1be23eb44ed9
rafalcl <rafalclab@gmail.com>
2018-04-27 16:43:01 +0200

    Add user controller

---
+++ src/pl/coderslab/warsztaty3/controller/user/EditUserController.java
@@ -0,0 +1,44 @@
+package pl.coderslab.warsztaty3.controller.user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddUserController
+ */
+@WebServlet("/user/edit")
+public class EditUserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EditUserController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/WEB-INF/views/user/edit-user-form.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// utworzyć nowego usera w DB używając DbUtils
+		doGet(request, response);
+	}
+
+}
